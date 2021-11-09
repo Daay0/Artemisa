065a51c19c821d6280146b23b695eb1751fe7401
Daay <Danielchavez126@hotmail.com>
2021-11-08 18:56:16 -0600

    filtro al cien

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
-var data_All = '', data_Question = '', query_string = '';
-
-
+var data_All,
+  data_Question = '',
+  query_string = '',
+  type = '',
+  filters = [];
 
 function getData(type) {
   data_All = null;
@@ -12,7 +14,6 @@ function getData(type) {
   fetch(url).then(res => res.json().then(
     function data(data) {
       data_All = data;
-      console.log(data_All);
       showData(data_All);
     }
   ).catch(function error() {
@@ -23,7 +24,7 @@ function getData(type) {
 function showData(data) {
   document.getElementById('cards').innerHTML = '';
   for (n = 0; n < data.length; n++) {
-    type_Data = (data[n].tipo == 0) ? 'Arbutus' : 'Comarostaphylis';
+    type_Data = (data[n].tipo == 'Arbutus') ? 'Arbutus' : 'Comarostaphylis';
     document.getElementById('cards').innerHTML += '<li>' +
       '<a href="#" class="card">' +
       '<img src="img/madroños/' + data[n].especie + '.jpg" class="card__image" alt="' + data[n].especie + '" />' +
@@ -55,37 +56,48 @@ function filterData() {
 }
 
 function selectFilters() {
-
+  for(m = 0; m < filters.length; m++){
+    if(query_string[filters[m]]){
+      for(n = 0; n < query_string[filters[m]].length; n++){
+        document.getElementById(query_string[filters[m]][n]).checked = true;
+      }
+    }
+  }
 }
 
 function getGalleryReady() {
   query_string = QueryStringToJSON();
-  type = query_string['tipo'];
+  type = query_string['tipo'][0];
   filter = (type == 'Arbutus') ? 1 : 2;
   getFilter(filter);
   activeLink(type);
   getData(type);
-  console.log(type);
-  console.log(query_string);
-  console.log(data_Question);
   filterData();
 }
 
 function QueryStringToJSON() {
   var pairs = location.search.slice(1).split('&');
   var result = {};
+  var tri = [];
   pairs.forEach(function(pair) {
     pair = pair.split('=');
+    // if (result[pair[0]]) result[pair[0]] += ';' + decodeURIComponent(pair[1] || '');
+    // else result[pair[0]] = decodeURIComponent(pair[1] || '');
     result[pair[0]] = decodeURIComponent(pair[1] || '');
+    var temp = pair[0];
+    if(tri[temp]) tri[temp].push(decodeURIComponent(pair[1] || ''));
+    else {
+      tri[temp] = [];
+      tri[temp].push(decodeURIComponent(pair[1] || ''));
+    }
   });
-
-  return JSON.parse(JSON.stringify(result));
+  return tri;
 }
 
-function activeLink(type){
-  if(type == 'Arbutus'){
+function activeLink(type) {
+  if (type == 'Arbutus') {
     document.getElementById('nav_Arbutus').classList.add('active-link');
-  }else if(type == 'Comarostaphylis'){
+  } else if (type == 'Comarostaphylis') {
     document.getElementById('nav_Comarostaphylis').classList.add('active-link');
   }
 }
@@ -94,20 +106,41 @@ function getFilter(type) {
   url = 'http://localhost:3000/task/buscar/' + type;
   fetch(url).then(res => res.json().then(
     function data(data) {
-      data_Question = data;
+      data_Question = '1';
       for (n = 0; n < data.length; n++) {
-        document.getElementById('fill_Filter').innerHTML += '<form class="form-container" id="' + data[n].pregunta + n + '">' +
-          '<h3 class="subtitle__main" >' + data[n].pregunta + '</h3>' +
+        filters.push(data[n].pregunta);
+        document.getElementById('fill_Filter').innerHTML += '<form class="form-container" id="' + data[n].pregunta + '">' +
+          '<h3 class="subtitle__main" >' + data[n].descripcion + '</h3>' +
           '</form>';
         for (m = 0; m < data[n].result.length; m++) {
-          document.getElementById(data[n].pregunta + n).innerHTML += '<label class="checkbox" id="' + data[n].result[m] + '">' +
-            '<input type="checkbox">' + data[n].result[m] +
+          document.getElementById(data[n].pregunta).innerHTML += '<label class="checkbox">' +
+            '<input type="checkbox" class="chk_All" id="' + data[n].result[m] + '" name="' + data[n].pregunta + '" value="' + data[n].result[m] + '">' + data[n].result[m] +
             '<span class="check"></span>' +
             '</label>';
         }
       }
+      selectFilters();
     }
+
   ).catch(function error() {
     alert('Fallo *getFilter()*');
   }));
+}
+
+
+function checkFilter() {
+  var chk_All = document.querySelectorAll('.chk_All');
+  var enabledSet =
+    Array.from(chk_All) // Convert checkboxes to an array to use filter and map.
+    .filter(i => i.checked) // Use Array.filter to remove unchecked checkboxes.
+    .map(i => [i.name + '=' + i.value]) // Use Array.map to extract only the checkbox values from the array of objects.
+  var send = '';
+  for (var j = 0; j < enabledSet.length; j++) {
+    send += enabledSet[j] + '&'
+  }
+  location.href = 'galeria.html?tipo=' + type + '&' + send;
+}
+
+function cleanFilter() {
+  location.href = 'galeria.html?tipo=' + type;
 }
